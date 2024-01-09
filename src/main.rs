db8a0de179d9bc6d29b015f678ee81541cb3b532
Chanyub.Park <mrchypark@gmail.com>
2024-01-09 10:32:34 +0000

    minimal version

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 use futures::StreamExt;
-use k8s_openapi::api::core::v1::Pod;
 use kube::{
 	runtime::controller::{Action, Controller},
 	Api, Client, ResourceExt,
 };
 use std::{sync::Arc, time::Duration};
+
+use crate::crd::{Client as RH_Client, Server as RH_Server};
+pub mod crd;
 
 #[derive(thiserror::Error, Debug)]
 pub enum Error {}
@@ -13,21 +15,36 @@ pub type Result<T, E = Error> = std::result::Result<T, E>;
 #[tokio::main]
 async fn main() -> Result<(), kube::Error> {
 	let client = Client::try_default().await?;
-	let pods = Api::<Pod>::all(client);
+	let srv: Api<RH_Server> = Api::all(client.clone());
+	let cli: Api<RH_Client> = Api::all(client.clone());
 
-	Controller::new(pods.clone(), Default::default())
-		.run(reconcile, error_policy, Arc::new(()))
-		.for_each(|_| futures::future::ready(()))
-		.await;
+	let srv_con = Controller::new(srv.clone(), Default::default())
+		.run(srv_reconcile, srv_error_policy, Arc::new(()))
+		.for_each(|_| futures::future::ready(()));
+
+	let cli_con = Controller::new(cli.clone(), Default::default())
+		.run(cli_reconcile, cli_error_policy, Arc::new(()))
+		.for_each(|_| futures::future::ready(()));
+
+	futures::join!(srv_con, cli_con);
 
 	Ok(())
 }
 
-async fn reconcile(obj: Arc<Pod>, _ctx: Arc<()>) -> Result<Action> {
-	println!("reconcile request: {}", obj.name_any());
+async fn srv_reconcile(obj: Arc<RH_Server>, _ctx: Arc<()>) -> Result<Action> {
+	println!("server request: {}", obj.name_any());
 	Ok(Action::requeue(Duration::from_secs(10)))
 }
 
-fn error_policy(_object: Arc<Pod>, _err: &Error, _ctx: Arc<()>) -> Action {
+fn srv_error_policy(_object: Arc<RH_Server>, _err: &Error, _ctx: Arc<()>) -> Action {
+	Action::requeue(Duration::from_secs(5))
+}
+
+async fn cli_reconcile(obj: Arc<RH_Client>, _ctx: Arc<()>) -> Result<Action> {
+	println!("client request: {}", obj.name_any());
+	Ok(Action::requeue(Duration::from_secs(10)))
+}
+
+fn cli_error_policy(_object: Arc<RH_Client>, _err: &Error, _ctx: Arc<()>) -> Action {
 	Action::requeue(Duration::from_secs(5))
 }
