433c4fb13e8449918fca33e991cbbb156e9c577d
Chanyub.Park <mrchypark@gmail.com>
2024-01-09 09:38:47 +0000

    add formmater

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
-use std::{sync::Arc, time::Duration};
 use futures::StreamExt;
 use k8s_openapi::api::core::v1::Pod;
 use kube::{
-    Api, Client, ResourceExt,
-    runtime::controller::{Action, Controller}
+	runtime::controller::{Action, Controller},
+	Api, Client, ResourceExt,
 };
+use std::{sync::Arc, time::Duration};
 
 #[derive(thiserror::Error, Debug)]
 pub enum Error {}
@@ -12,23 +12,22 @@ pub type Result<T, E = Error> = std::result::Result<T, E>;
 
 #[tokio::main]
 async fn main() -> Result<(), kube::Error> {
-  println!("start");
-    let client = Client::try_default().await?;
-    let pods = Api::<Pod>::all(client);
+	let client = Client::try_default().await?;
+	let pods = Api::<Pod>::all(client);
 
-    Controller::new(pods.clone(), Default::default())
-        .run(reconcile, error_policy, Arc::new(()))
-        .for_each(|_| futures::future::ready(()))
-        .await;
+	Controller::new(pods.clone(), Default::default())
+		.run(reconcile, error_policy, Arc::new(()))
+		.for_each(|_| futures::future::ready(()))
+		.await;
 
-    Ok(())
+	Ok(())
 }
 
 async fn reconcile(obj: Arc<Pod>, _ctx: Arc<()>) -> Result<Action> {
-    println!("reconcile request: {}", obj.name_any());
-    Ok(Action::requeue(Duration::from_secs(10)))
+	println!("reconcile request: {}", obj.name_any());
+	Ok(Action::requeue(Duration::from_secs(10)))
 }
 
 fn error_policy(_object: Arc<Pod>, _err: &Error, _ctx: Arc<()>) -> Action {
-    Action::requeue(Duration::from_secs(5))
+	Action::requeue(Duration::from_secs(5))
 }
