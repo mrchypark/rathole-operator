ec7ec43bb3b35423923c09b31749e8e3c34d826a
Chanyub.Park <mrchypark@gmail.com>
2024-01-09 08:22:24 +0000

    base struct for crd

--- src/crd.rs
+++ src/crd.rs
@@ -2,7 +2,7 @@ use kube::CustomResource;
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
-#[derive(CustomResource, Serialize, Deserialize, Debug, PartialEq, Clone, JsonSchema)]
+#[derive(CustomResource, Serialize, Deserialize, Default, Debug, PartialEq, Clone, JsonSchema)]
 #[kube(
     group = "rathole.mrchypark.github.io",
     version = "v1alpha1",
@@ -13,7 +13,60 @@ use serde::{Deserialize, Serialize};
 )]
 #[kube(status = "ClientStatus")]
 pub struct ClientSpec {
-    pub replicas: i32,
+  #[serde(rename(deserialize = "serverRef"))]
+  pub server_ref: String,
+  #[serde(rename(deserialize = "configTo"))]
+  pub config_to: ClientConfig,
+  /// Timeout for the heartbeat signal in seconds.
+  #[serde(default = "heartbeat_timeout_default")]
+  #[serde(rename(deserialize = "heartbeatTimeout"))]
+  pub heartbeat_timeout: i32,
+  #[serde(default = "retry_interval_default")]
+  #[serde(rename(deserialize = "retryInterval"))]
+  pub retry_interval: i32,
+  pub services: Vec<ServiceConfig>,
+}
+
+#[derive(Serialize, Deserialize, Default, Debug, PartialEq, Clone, JsonSchema)]
+pub struct  ClientConfig{
+  #[serde(default = "client_config_type_defualt")]
+  pub r#type: ClientConfigType,
+  #[serde(skip_serializing_if = "Option::is_none")]
+  pub name: Option<String>,
+  #[serde(skip_serializing_if = "Option::is_none")]
+  pub namespace: Option<String>,
+}
+
+#[derive(Serialize, Deserialize, Debug, Default, PartialEq, Clone, JsonSchema)]
+#[serde(rename_all = "lowercase")]
+pub enum ClientConfigType {
+  #[default] Secret,
+}
+
+fn client_config_type_defualt() -> ClientConfigType {
+  ClientConfigType::Secret
+}
+
+#[derive(Serialize, Deserialize, Default, Debug, PartialEq, Clone, JsonSchema)]
+pub struct ServiceConfig {
+  pub name: String,
+  #[serde(rename(deserialize = "localAddr"))]
+  pub local_addr: String, 
+  pub r#type: TransportType,
+  #[serde(default = "nodelay_default")]
+  pub nodelay: bool,
+  #[serde(default = "retry_interval_default")]
+  #[serde(rename(deserialize = "retryInterval"))]
+  pub retry_interval: i32,
+  pub token: Token,
+}
+
+fn nodelay_default() -> bool {
+  true
+}
+
+fn retry_interval_default() -> i32 {
+  1
 }
 
 #[derive(Serialize, Deserialize, Debug, PartialEq, Clone, JsonSchema)]
@@ -21,21 +74,96 @@ pub struct ClientStatus {
   pub is_ok: bool,
 }
 
-#[derive(CustomResource, Serialize, Deserialize, Debug, PartialEq, Clone, JsonSchema)]
+#[derive(CustomResource, Serialize, Deserialize, Default, Debug, PartialEq, Clone, JsonSchema)]
 #[kube(
     group = "rathole.mrchypark.github.io",
     version = "v1alpha1",
     kind = "Server",
     plural = "servers",
     derive = "PartialEq",
+    derive = "Default",
     namespaced
 )]
 #[kube(status = "ServerStatus")]
 pub struct ServerSpec {
-    pub replicas: i32,
+  #[serde(rename(deserialize = "bindAddr"))]
+  pub bind_addr: String,
+  #[serde(rename(deserialize = "defaultToken"))]
+  pub default_token: String,
+  #[serde(rename(deserialize = "exposeAddr"))]
+  pub expose_addr: String,
+  #[serde(default = "heartbeat_interval_default")]
+  #[serde(rename(deserialize = "heartbeatInterval"))]
+  pub heartbeat_interval: i32,
+  pub transport: Transport,
 }
 
-#[derive(Serialize, Deserialize, Debug, PartialEq, Clone, JsonSchema)]
+fn heartbeat_interval_default() -> i32 {
+  30
+}
+
+fn heartbeat_timeout_default() -> i32 {
+  30
+}
+
+
+#[derive(Serialize, Deserialize, Debug, Default, PartialEq, Clone, JsonSchema)]
+pub struct Transport {
+  pub r#type: TransportType,
+  #[serde(skip_serializing_if = "Option::is_none")]
+  pub tcp: Option<String>,
+  #[serde(skip_serializing_if = "Option::is_none")]
+  pub tls: Option<String>,
+  #[serde(skip_serializing_if = "Option::is_none")]
+  pub websocket: Option<String>,
+  #[serde(skip_serializing_if = "Option::is_none")]
+  pub noise: Option<NoiseConfig>,
+}
+
+#[derive(Serialize, Deserialize, Debug,Default, PartialEq, Clone, JsonSchema)]
+pub struct NoiseConfig {
+  #[serde(default = "noise_pattern_default")]
+  pub pattern: String,
+  pub noisekey: Token,
+}
+
+fn noise_pattern_default() -> String {
+  "Noise_NK_25519_ChaChaPoly_BLAKE2s".into()
+}
+
+#[derive(Serialize, Deserialize, Debug, Default, PartialEq, Clone, JsonSchema)]
+pub struct Token {
+  pub r#type: SecretType,
+  #[serde(skip_serializing_if = "Option::is_none")]
+  pub key: Option<String>,
+  #[serde(skip_serializing_if = "Option::is_none")]
+  #[serde(rename(deserialize = "secretRef"))]
+  pub secret_ref: Option<SecretRef>,
+}
+
+#[derive(Serialize, Deserialize, Debug, Default, PartialEq, Clone, JsonSchema)]
+pub struct SecretRef {
+  pub name: String,
+  pub key: String,
+}
+
+#[derive(Serialize, Deserialize, Debug, Default, PartialEq, Clone, JsonSchema)]
+#[serde(rename_all = "lowercase")]
+pub enum TransportType {
+  #[default] Noise,
+    TCP,
+    TLS,
+    Websocket,
+}
+
+#[derive(Serialize, Deserialize, Debug, Default, PartialEq, Clone, JsonSchema)]
+#[serde(rename_all = "lowercase")]
+pub enum SecretType {
+    Direct,
+    #[default] Reference,
+}
+
+#[derive(Serialize, Deserialize, Debug, Default, PartialEq, Clone, JsonSchema)]
 pub struct ServerStatus {
   pub is_ok: bool,
 }
